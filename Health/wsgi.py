12a66bbe058aee48a6348eda9a870cf9fc4d1c5c
zhenyuanwei <zhenyuanwei@foxmail.com>
2016-07-17 17:11:44 +0800

    update the config fro wsgi-mod

--- Health/wsgi.py
+++ Health/wsgi.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import sys
+path = '/var/www/Health'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Health.settings")
 
