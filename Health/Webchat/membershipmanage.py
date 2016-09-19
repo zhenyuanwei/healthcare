dc15f36b1adc65adf3cef21652813f1636dc22cc
zhenyuanwei <zhenyuanwei@foxmail.com>
2016-09-19 20:56:31 +0800

    bug fixing & add payment log for webchat

--- Health/Webchat/membershipmanage.py
+++ Health/Webchat/membershipmanage.py
@@ -6,8 +6,12 @@ Created on Jul 28, 2016
 from django.http import HttpResponse
 from django.template.loader import get_template
 from Health.Webchat.myweixin import getOpenID
-from HealthModel.models import Membership
+from HealthModel.models import Membership, Transaction
 from Health.formatValidation import phoneNumberCheck
+from Health.Admin.payment import createPayment
+
+maxCount = 5 
+
 def getMembership(openId):
     membership = Membership.objects.get(webchatid=openId)
     return membership
@@ -24,13 +28,18 @@ def bindMembershipCheck(request):
         openId = getOpenID(code=code)
     except :
         openId = '000'
-        print '------------this user is not binded-----------'
+        print '------------this user is not binded-------- ' + openId
     #get webchat user
     try :
         membership = getMembership(openId=openId)
-        usedTemplate = get_template('webchat/membershipinfo.html')
         membershipDic = {'membership' : membership}
+        
+        paymentList = getPaymentLog(membershipId = membership.id)
+        membershipDic['paymentList'] = paymentList
+        
+        usedTemplate = get_template('webchat/membershipinfo.html')
         html = usedTemplate.render(membershipDic)
+        
         return HttpResponse(html)
     except :
         outDic = {}
@@ -46,13 +55,17 @@ def bindMembership(request):
     membershipDic = {}
     try :
         membership = getMembership2(vipno=vipno, phonenumber=phonenumber)
+        membershipDic['membership'] = membership
         if membership.webchatid == '' :
             membership.webchatid = openId
             membership.save()
         else :
             membershipDic['messages'] = 'BINDED'
+        
+        paymentList = getPaymentLog(membershipId = membership.id)
+        membershipDic['paymentList'] = paymentList
+        
         usedTemplate = get_template('webchat/membershipinfo.html')
-        membershipDic['membership'] = membership
         html = usedTemplate.render(membershipDic)
         return HttpResponse(html)
     except :
@@ -61,7 +74,21 @@ def bindMembership(request):
         html = usedTemplate.render(messageDic)
         return HttpResponse(html)
         
-    
-    
+def getPaymentLog(membershipId):
+    paymentList = []
+    transactionList = Transaction.objects.filter(membershipId = membershipId).order_by('transactionDate')
+    count = len(transactionList)
+    if count >= maxCount :
+        count = maxCount
+    i = 0
+    for transaction in transactionList :
+        if i < count :
+            payment = createPayment(transaction = transaction)
+            paymentList.append(payment)
+            i = i + 1
+        else :
+            break
+        
+    return paymentList
     
     
